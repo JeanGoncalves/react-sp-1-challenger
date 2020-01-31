908eeec02c3f505ad6dbcadfde4b3d3ac6665227
Jean Gonçalves <jgoncalves@uolinc.com>
2020-01-31 18:00:50 -0300

    Update code

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ var fs = require('fs')
 var dotenv = require('dotenv')
 dotenv.config()
 
+const CONSTANT = require('./constants.json')
+
 const token = process.env.TOKEN
-const fileName = 'answer.json'
+const fileName = 'archive/answer.json'
 
 var options = {
-    host: "api.codenation.dev",
-    path: "/v1/challenge/dev-ps/generate-data?token=" + token,
+    host: CONSTANT.host,
+    path: CONSTANT.path + token,
     method: "GET",
     port: 443
 };
@@ -17,11 +19,12 @@ var options = {
 var req = https.get(options, function(res) {  
     res.on('data', function(data) {
         let response = JSON.parse(data);
-        createFile(fileName, JSON.stringify(response, null, '\t'))
-
         response.decifrado = decrypt(response.numero_casas, response.cifrado)
         response.resumo_criptografico = sha1(response.decifrado)
-
+        fs.writeFile(CONSTANT.file, JSON.stringify(response, null, '\t'), function (err) {
+            if (err) throw err;
+            console.log('File is created successfully.');
+        })
     });
 });
 
@@ -32,14 +35,9 @@ req.on('error', function(e) {
 
 req.end();
 
-
 function decrypt (key, value) {
     const alphabet = Array.from({length: 26}, (v, i) => String.fromCharCode(i + 97))
     return value.split('')
         .map(letter => alphabet.indexOf(letter) == -1 ? letter : alphabet[alphabet.indexOf(letter) - key])
         .join('')
-}
-
-function createFile (name, content) {
-    fs.writeFile(name, content)
 }
